13f42ca494d08022dbf98d8854308c514af6ea2a
Thameem <thameemc17@gmail.com>
2022-06-27 14:12:00 +0400

    Swipe button component completed

--- tinder-clone/src/components/SwipeButton.js
+++ tinder-clone/src/components/SwipeButton.js
@@ -1,9 +1,30 @@
 import React from "react";
+import ReplayIcon from "@mui/icons-material/Replay";
+import CloseIcon from "@mui/icons-material/Close";
+import StarIcon from "@mui/icons-material/Star";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import IconButton from "@mui/material/IconButton";
+import "./SwipeButton.css"; 
 
 function SwipeButton() {
 	return (
-		<div className="Swipe-Button">
-			<h1>Swipe Button</h1>
+		<div className="Swipe-button">
+			<IconButton className="Swipe-button-repeat">
+				<ReplayIcon />
+			</IconButton>
+			<IconButton className="Swipe-button-left">
+				<CloseIcon />
+			</IconButton>
+			<IconButton className="Swipe-button-star">
+				<StarIcon />
+			</IconButton>
+			<IconButton className="Swipe-button-right">
+				<FavoriteIcon />
+			</IconButton>
+			<IconButton className="Swipe-button-lightning">
+				<FlashOnIcon />
+			</IconButton>
 		</div>
 	);
 }
